62970e79e048f6d62d0feb15021cb6484a32e3e4
Alex Eyhorn <alex.eyhorn@planetinnovation.com.au>
2019-03-01 12:05:20 +1100

    Added the powershel script module and the readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# share-octopus-variables
-Share Octopus Deploy variables between projects
+# Share Octopus Deploy variables between projects
+
+There is no straightforward way to use transformed variables from one project in another project in [Octopus Deploy](https://octopus.com/).
+The solution is to use Octopus API.
+
+## Variable sharing guide
+
+1. In the library create script module `ShareVariables` using content of the shareProjectVariablesModule.ps1 as the body for the module
+2. In the source project:
+   1. Include script module `ShareVariables`
+   2. Add a script step `Set Shared Variables`
+   3. As a script body use `Set-SharedVariables`
+3. In the target deployment project
+   1. Include script module `ShareVariables`
+   2. For each step that requires shared variables:
+      1. Enable `Custom Deployment scripts` in [Configuration features](https://octopus.com/docs/deployment-process/configuration-features)
+      2. In the `Pre-deployment script` set the body of the script to `Read-SharedVariables`
+
+## Configuration of the script module
+
+In the `ShareVariables` script module set:
+
+1. In the function `Get-SharedVariableNames` provide names of the variables that require sharing
+2. In the function `Read-SharedVariables`:
+   1. Set `$projectName` to the name of project you are reading variables from
+   2. Set `$octopusApiKey` to the [Api key](https://octopus.com/docs/api-and-integration/api/how-to-create-an-api-key) that you have created
+
+If you do not use tenants in your deployments you might need to remove `tenant` url parameter from the `$getDeploymentUrl`
---
+++ shareProjectVariablesModule.ps1
@@ -0,0 +1,30 @@
+function Get-SharedVariableNames {
+    return @("VariableName1", "VariableName2", "VariableName3")
+}
+
+function Set-SharedVariables {
+    $variablesToSet = Get-SharedVariableNames
+    foreach ($variableName in $variablesToSet) {
+        Set-OctopusVariable -name "Shared.$($variableName)" -value $OctopusParameters[$variableName]
+    }
+}
+
+function Read-SharedVariables {
+    $octopusURL = $OctopusParameters["Octopus.Web.BaseUrl"]
+    $projectName = "name of the project you a reading variables from"
+    $environment = $OctopusParameters["Octopus.Environment.Id"]
+    $tenant = $OctopusParameters["Octopus.Deployment.Tenant.Id"]
+    $octopusApiKey = "use you Octopus api key"
+    $header = @{ "X-Octopus-ApiKey" = octopusApiKey }
+  
+    $project = (Invoke-RestMethod $octopusURL/api/projects?name=$projectName -Headers $header -UseBasicParsing).Items | Where-Object Name -eq $projectName
+    $getDeploymentUrl = "$($octopusURL)/api/deployments?projects=$($project.Id)&environments=$($environment)&tenants=$($tenant)&taskState=success&take=1"
+    $deployment = (Invoke-RestMethod $getDeploymentUrl -Headers $header -UseBasicParsing).Items | Select-Object -first 1
+    $variables = (Invoke-RestMethod $octopusURL/api/variables/variableset-$($deployment.Id) -Headers $header -UseBasicParsing).Variables
+  
+    $variablesToSet = Get-SharedVariableNames
+    foreach ($variableName in $variablesToSet) {
+        $variable = $variables | Where-Object Name -Like "*.Output.Shared.$($variableName)" | Select-Object -first 1
+        Set-OctopusVariable -name $variableName -value "$($variable.Value)"  
+    }
+  }
