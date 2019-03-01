684e26bda8726883f5e26663e3bf55ee0d18daee
Alex Eyhorn <alex.eyhorn@planetinnovation.com.au>
2019-03-01 15:01:00 +1100

    Pointing readme at the blog post and cleaning formatting

--- shareProjectVariablesModule.ps1
+++ shareProjectVariablesModule.ps1
@@ -16,15 +16,15 @@ function Read-SharedVariables {
     $tenant = $OctopusParameters["Octopus.Deployment.Tenant.Id"]
     $octopusApiKey = "use you Octopus api key"
     $header = @{ "X-Octopus-ApiKey" = octopusApiKey }
-  
+
     $project = (Invoke-RestMethod $octopusURL/api/projects?name=$projectName -Headers $header -UseBasicParsing).Items | Where-Object Name -eq $projectName
     $getDeploymentUrl = "$($octopusURL)/api/deployments?projects=$($project.Id)&environments=$($environment)&tenants=$($tenant)&taskState=success&take=1"
     $deployment = (Invoke-RestMethod $getDeploymentUrl -Headers $header -UseBasicParsing).Items | Select-Object -first 1
     $variables = (Invoke-RestMethod $octopusURL/api/variables/variableset-$($deployment.Id) -Headers $header -UseBasicParsing).Variables
-  
+
     $variablesToSet = Get-SharedVariableNames
     foreach ($variableName in $variablesToSet) {
         $variable = $variables | Where-Object Name -Like "*.Output.Shared.$($variableName)" | Select-Object -first 1
-        Set-OctopusVariable -name $variableName -value "$($variable.Value)"  
+        Set-OctopusVariable -name $variableName -value "$($variable.Value)"
     }
   }
